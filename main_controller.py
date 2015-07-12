e3a3560296dfcd8773dee33faf9fa8f0e98798aa
Raphael Vogel <raphael.vogel@web.de>
2015-07-12 21:59:40 +0200

    start with multiprocessing

--- main_controller.py
+++ main_controller.py
@@ -1,1 +1,18 @@
-__author__ = 'raphael'
+import multiprocessing as mp
+import queue
+import modules.rpm_controller as rpm
+import time
+
+
+rpm_queue = mp.Queue(1)
+p_rpm = mp.Process(target=rpm.get_rpm, args=(rpm_queue,))
+p_rpm.daemon = True
+p_rpm.start()
+
+# main loop
+while True:
+    try:
+        print(rpm_queue.get_nowait())
+    except queue.Empty:
+        print("Queue was empty")
+    time.sleep(0.5)
