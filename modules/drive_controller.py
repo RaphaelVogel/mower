ff7d348be89267138d60195c3aeac4e52b57e557
Raphael Vogel <raphael.vogel@web.de>
2015-07-28 22:19:03 +0200

    tune rpm moving average

--- modules/drive_controller.py
+++ modules/drive_controller.py
@@ -4,111 +4,108 @@ from tinkerforge.ip_connection import IPConnection
 from tinkerforge.brick_servo import Servo
 from tinkerforge.bricklet_io16 import IO16
 
+ipcon = IPConnection()
+
+cur_mode = 'stop'
+cur_speed = 0
+
+rpm_values_right = deque(maxlen=8)  # queue for calculating moving average
+rpm_values_left = deque(maxlen=8)  # queue for calculating moving average
+
+loop_counter = 0
+internal_cmd = None
+
 
 def start(main_conn):
-    ipcon = IPConnection()
+    global loop_counter, internal_cmd, rpm_values_right, rpm_values_left
     ipcon.connect('localhost', 4223)
-
     servo = Servo('6QFwhz', ipcon)
     servo.set_acceleration(0, 50000)  # right wheel
     servo.set_acceleration(1, 50000)  # left wheel
     servo.set_acceleration(2, 30000)  # cutter
-
     io16 = IO16('b7Y', ipcon)
     io16.set_port_configuration('a', 0b11111111, IO16.DIRECTION_IN, True)  # all pins input with pull-up
     io16.set_edge_count_config(0, IO16.EDGE_TYPE_BOTH, 1)  # set pin 0 for edge count
     io16.set_edge_count_config(1, IO16.EDGE_TYPE_BOTH, 1)  # set pin 1 for edge count
 
-    curr_mode = 'stop'
-    curr_value = 0
-    rpm_values_right = deque(maxlen=10)
-    rpm_values_left = deque(maxlen=10)
-
-    count_rpm = 0
-    internal_cmd = None
     while True:
-        count_rpm += 1
-        if count_rpm > 10000:
-            count_rpm = 0
+        loop_counter += 1
+        if loop_counter > 10000:
+            loop_counter = 0
 
         # Commands: forward/<speed>, backward/<speed>, turnL/, turnR/,
         # curveL/<smooth|medium|strong>, curveR/<smooth|medium|strong>, cutter/<speed>, stop/, terminate/
-        if main_conn.poll() or internal_cmd is not None:
-            if internal_cmd is not None:
-                cmd = internal_cmd
-                internal_cmd = None
-            else:
-                cmd = main_conn.recv()
-
-            split_cmd = cmd.split('/')
-            cur_mode = split_cmd[0]
-            if cur_mode == 'forward':
-                curr_value = int(split_cmd[1])
-                rpm_values_right.clear()
-                rpm_values_left.clear()
-                execute_drive_command(servo, curr_value, curr_value)
-            elif cur_mode == 'backward':
-                curr_value = int(split_cmd[1])
-                rpm_values_right.clear()
-                rpm_values_left.clear()
-                execute_drive_command(servo, -curr_value, -curr_value)
-            elif cur_mode == 'turnL':
-                rpm_values_right.clear()
-                rpm_values_left.clear()
-                curr_value = 3000
-                execute_drive_command(servo, curr_value, -curr_value)
-            elif cur_mode == 'turnR':
-                rpm_values_right.clear()
-                rpm_values_left.clear()
-                curr_value = 3000
-                execute_drive_command(servo, -curr_value, curr_value)
-            elif cur_mode == 'curveL':
-                rpm_values_left.clear()
-                if split_cmd[1] == 'smooth':
-                    execute_drive_command(servo, curr_value, curr_value - 400)
-                elif split_cmd[1] == 'medium':
-                    execute_drive_command(servo, curr_value, curr_value - 700)
-                elif split_cmd[1] == 'strong':
-                    execute_drive_command(servo, curr_value, curr_value - 1000)
-            elif cur_mode == 'curveR':
-                rpm_values_right.clear()
-                if split_cmd[1] == 'smooth':
-                    execute_drive_command(servo, curr_value - 400, curr_value)
-                elif split_cmd[1] == 'medium':
-                    execute_drive_command(servo, curr_value - 700, curr_value)
-                elif split_cmd[1] == 'strong':
-                    execute_drive_command(servo, curr_value - 1000, curr_value)
-            elif cur_mode == 'cutter':
-                execute_cutter_command(servo, int(split_cmd[1]))
-            elif cur_mode == 'stop':
-                rpm_values_right.clear()
-                rpm_values_left.clear()
-                execute_drive_command(servo, 0, 0)
-            elif cur_mode == 'terminate':
-                ipcon.disconnect()
-                break
+        if main_conn.poll():
+            cmd = main_conn.recv()
+            execute_command(cmd, servo)
+        elif internal_cmd is not None:
+            cmd = internal_cmd
+            internal_cmd = None
+            execute_command(cmd, servo)
 
         # check rpm per wheel against moving average to check if wheel is blocked
-        if (count_rpm % 4) == 0:
+        if (loop_counter % 5) == 0:
             right_rpm = io16.get_edge_count(0, True)
             left_rpm = io16.get_edge_count(1, True)
+            rpm_values_right.append(right_rpm)
+            rpm_values_left.append(left_rpm)
+
             if len(rpm_values_right) > 3:
                 moving_average = sum(rpm_values_right) / len(rpm_values_right)
-                if moving_average * 0.8 > right_rpm:
+                if right_rpm < (moving_average * 0.85):
                     internal_cmd = 'stop/'
-                rpm_values_right.append(right_rpm)
-            else:
-                rpm_values_right.append(right_rpm)
 
             if len(rpm_values_left) > 3:
                 moving_average = sum(rpm_values_left) / len(rpm_values_left)
-                if moving_average * 0.8 > left_rpm:
+                if left_rpm < (moving_average * 0.85):
                     internal_cmd = 'stop/'
-                rpm_values_left.append(left_rpm)
-            else:
-                rpm_values_left.append(left_rpm)
 
         time.sleep(0.02)
+
+
+def execute_command(cmd, servo):
+    global cur_mode, cur_speed
+    split_cmd = cmd.split('/')
+    cur_mode = split_cmd[0]
+    if cur_mode == 'forward':
+        cur_speed = int(split_cmd[1])
+        reset_queues('both')
+        execute_drive_command(servo, cur_speed, cur_speed)
+    elif cur_mode == 'backward':
+        cur_speed = int(split_cmd[1])
+        reset_queues('both')
+        execute_drive_command(servo, -cur_speed, -cur_speed)
+    elif cur_mode == 'turnL':
+        reset_queues('both')
+        cur_speed = 3000
+        execute_drive_command(servo, cur_speed, -cur_speed)
+    elif cur_mode == 'turnR':
+        reset_queues('both')
+        cur_speed = 3000
+        execute_drive_command(servo, -cur_speed, cur_speed)
+    elif cur_mode == 'curveL':
+        reset_queues('left')
+        if split_cmd[1] == 'smooth':
+            execute_drive_command(servo, cur_speed, cur_speed - 400)
+        elif split_cmd[1] == 'medium':
+            execute_drive_command(servo, cur_speed, cur_speed - 700)
+        elif split_cmd[1] == 'strong':
+            execute_drive_command(servo, cur_speed, cur_speed - 1000)
+    elif cur_mode == 'curveR':
+        reset_queues('right')
+        if split_cmd[1] == 'smooth':
+            execute_drive_command(servo, cur_speed - 400, cur_speed)
+        elif split_cmd[1] == 'medium':
+            execute_drive_command(servo, cur_speed - 700, cur_speed)
+        elif split_cmd[1] == 'strong':
+            execute_drive_command(servo, cur_speed - 1000, cur_speed)
+    elif cur_mode == 'cutter':
+        execute_cutter_command(servo, int(split_cmd[1]))
+    elif cur_mode == 'stop':
+        reset_queues('both')
+        execute_drive_command(servo, 0, 0)
+    elif cur_mode == 'terminate':
+        ipcon.disconnect()
 
 
 # execute a drive command
@@ -122,3 +119,14 @@ def execute_drive_command(servo, right_wheel, left_wheel):
 def execute_cutter_command(servo, cutter_speed):
     servo.set_position(2, cutter_speed)
     servo.enable(2)
+
+
+def reset_queues(name):
+    global rpm_values_right, rpm_values_left
+    if name == 'right':
+        rpm_values_right.clear()
+    elif name == 'left':
+        rpm_values_left.clear()
+    elif name == 'both':
+        rpm_values_right.clear()
+        rpm_values_left.clear()
