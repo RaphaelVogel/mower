d3d6a858aab3f196dca84ac7db36af085d62b0aa
RaphaelVogel <raphael.vogel@icloud.com>
2015-12-17 22:04:09 +0100

    add bumper control

--- modules/drive_controller.py
+++ modules/drive_controller.py
@@ -6,6 +6,7 @@ from tinkerforge.ip_connection import IPConnection
 from tinkerforge.brick_servo import BrickServo
 from tinkerforge.bricklet_io16 import BrickletIO16
 from tinkerforge.bricklet_industrial_quad_relay import BrickletIndustrialQuadRelay
+from tinkerforge.bricklet_analog_in import BrickletAnalogIn
 import logging
 
 logger = logging.getLogger("mower_logger")
@@ -14,8 +15,9 @@ ipcon = IPConnection()
 cur_mode = 'stop'
 cur_speed = 0
 
-rpm_values_right = deque(maxlen=10)  # queue for calculating moving average
-rpm_values_left = deque(maxlen=10)  # queue for calculating moving average
+rpm_values_right = deque(maxlen=10)  # queue for calculating moving average of right wheel
+rpm_values_left = deque(maxlen=10)  # queue for calculating moving average of left wheel
+bumper_values = deque(maxlen=10)  # queue for calculating moving average of bumper values
 
 
 def signal_handler(signal_type, frame):
@@ -47,46 +49,59 @@ def start(parent_conn):
     io16.set_edge_count_config(1, BrickletIO16.EDGE_TYPE_BOTH, 1)  # set pin 1 for edge count
     iqr = BrickletIndustrialQuadRelay('mT2', ipcon)
     iqr.set_monoflop(0b0111, 0b0111, 1500)
+    analog = BrickletAnalogIn('', ipcon)
+    analog.set_range(BrickletAnalogIn.RANGE_UP_TO_6V)
 
     while True:
         loop_counter += 1
-        if loop_counter > 10000:
+        if loop_counter > 30000:
             loop_counter = 0
 
-        # Commands: forward/<speed>, backward/<speed>, turnL/, turnR/,
+        # commands: forward/<speed>, backward/<speed>, turnL/, turnR/,
         # curveL/<smooth|medium|strong>, curveR/<smooth|medium|strong>, cutter/<speed>, stop/
-        if parent_conn.poll():
-            cmd = parent_conn.recv()
-            logger.info("Execute external command %s" % cmd)
-            execute_command(cmd, servo)
-        elif internal_cmd is not None:
+        if internal_cmd is not None:
             cmd = internal_cmd
             internal_cmd = None
             logger.info("Execute internal command %s" % cmd)
             execute_command(cmd, servo)
+        elif parent_conn.poll():
+            cmd = parent_conn.recv()
+            logger.info("Execute external command %s" % cmd)
+            execute_command(cmd, servo)
 
-        # check rpm per wheel against moving average to check if wheel is blocked every 100ms
-        if (loop_counter % 5) == 0:
+        # check rpm per wheel against moving average to check if wheel is blocked
+        if (loop_counter % 8) == 0:
             right_rpm = io16.get_edge_count(0, True)
             left_rpm = io16.get_edge_count(1, True)
             rpm_values_right.append(right_rpm)
             rpm_values_left.append(left_rpm)
 
-            if len(rpm_values_right) > 3:
+            if len(rpm_values_right) > 5:  # omit first vales
                 moving_average = sum(rpm_values_right) / len(rpm_values_right)
                 if right_rpm < (moving_average * 0.50):
+                    logger.info("Right wheel blocked, stop mower")
                     internal_cmd = 'stop/'
 
-            if len(rpm_values_left) > 3:
+            if len(rpm_values_left) > 5:  # omit first vales
                 moving_average = sum(rpm_values_left) / len(rpm_values_left)
                 if left_rpm < (moving_average * 0.50):
+                    logger.info("Right wheel blocked, stop mower")
                     internal_cmd = 'stop/'
 
-        # update drive monoflop every second
-        if (loop_counter % 50) == 0:
+        # check bumper
+        if ((loop_counter + 4) % 8) == 0:
+            volt = analog.get_voltage()
+            bumper_values.append(volt)
+            moving_average = sum(bumper_values) / len(bumper_values)
+            if volt > (moving_average * 1.3):
+                logger.info("Bumper activated, stop mower")
+                internal_cmd = 'stop/'
+
+        # update drive monoflop
+        if (loop_counter % 100) == 0:
             iqr.set_monoflop(0b0111, 0b0111, 1500)
 
-        time.sleep(0.02)
+        time.sleep(0.01)
 
 
 def execute_command(cmd, servo):
