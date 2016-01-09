17ef990e71c97597b190b16e764c3931d6170391
RaphaelVogel <raphael.vogel@icloud.com>
2016-01-09 21:39:41 +0100

    fix rpm check

--- main_controller.py
+++ main_controller.py
@@ -1,5 +1,5 @@
 import multiprocessing as mp
-import modules.shutdown_controller as shutdown
+#import modules.shutdown_controller as shutdown
 import modules.drive_controller as drive
 import time
 from tinkerforge.ip_connection import IPConnection
@@ -20,10 +20,10 @@ logger.addHandler(filehandler)
 
 if __name__ == "__main__":
     # start shutdown controller
-    shutdown_conn, shutdown_conn1 = mp.Pipe()
-    p_shutdown = mp.Process(target=shutdown.start, args=(shutdown_conn1,), name="Shutdown Process")
-    p_shutdown.daemon = True
-    p_shutdown.start()
+    #shutdown_conn, shutdown_conn1 = mp.Pipe()
+    #p_shutdown = mp.Process(target=shutdown.start, args=(shutdown_conn1,), name="Shutdown Process")
+    #p_shutdown.daemon = True
+    #p_shutdown.start()
 
     # start drive controller
     drive_conn, drive_conn1 = mp.Pipe()
@@ -36,16 +36,35 @@ if __name__ == "__main__":
     time.sleep(0.5)
     master = BrickMaster('5Wr87j', ipcon)
 
-    while True:
-        if shutdown_conn.poll():
-            master.reset()
-            cmd = shutdown_conn.recv()
-            if cmd == "reboot":
-                subprocess.call("sudo reboot", shell=True)
-            elif cmd == "shutdown":
-                subprocess.call("sudo shutdown -h now", shell=True)
-            break
+    time.sleep(8)
+    drive_conn.send("forward/3500")
 
-        time.sleep(0.02)
+    while True:
+        #if shutdown_conn.poll():
+        #    master.reset()
+        #    cmd = shutdown_conn.recv()
+        #    if cmd == "reboot":
+        #        subprocess.call("sudo reboot", shell=True)
+        #    elif cmd == "shutdown":
+        #        subprocess.call("sudo shutdown -h now", shell=True)
+        #    break
+
+        if drive_conn.poll():
+            cmd = drive_conn.recv()
+            if cmd == "bumper_active":
+                time.sleep(0.5)
+                drive_conn.send("backward/3500")
+                time.sleep(3)
+                drive_conn.send("stop/")
+                time.sleep(0.5)
+                drive_conn.send("turnL/")
+                time.sleep(0.1)
+                drive_conn.send("reset_bumper/")
+                time.sleep(3)
+                drive_conn.send("stop/")
+                time.sleep(0.5)
+                drive_conn.send("forward/3500")
+
+        time.sleep(0.01)
 
     sys.exit(0)
--- modules/drive_controller.py
+++ modules/drive_controller.py
@@ -8,16 +8,20 @@ from tinkerforge.bricklet_io16 import BrickletIO16
 from tinkerforge.bricklet_industrial_quad_relay import BrickletIndustrialQuadRelay
 from tinkerforge.bricklet_analog_in import BrickletAnalogIn
 import logging
+from threading import Timer
 
 logger = logging.getLogger("mower_logger")
 ipcon = IPConnection()
 
+# globals
 cur_mode = 'stop'
 cur_speed = 0
+bumper_active = False
+rpm_activated = False
 
-rpm_values_right = deque(maxlen=10)  # queue for calculating moving average of right wheel
-rpm_values_left = deque(maxlen=10)  # queue for calculating moving average of left wheel
-bumper_values = deque(maxlen=10)  # queue for calculating moving average of bumper values
+rpm_values_right = deque(maxlen=20)  # queue for calculating moving average of right wheel
+rpm_values_left = deque(maxlen=20)  # queue for calculating moving average of left wheel
+bumper_values = deque(maxlen=20)  # queue for calculating moving average of bumper values
 
 
 def signal_handler(signal_type, frame):
@@ -29,18 +33,14 @@ def signal_handler(signal_type, frame):
 
 def start(parent_conn):
     loop_counter = 0
-    internal_cmd = None
+    internal_cmd = 'stop/'
     signal.signal(signal.SIGTERM, signal_handler)
     logger.info("Starting drive_controller")
-    global rpm_values_right, rpm_values_left
+    global rpm_values_right, rpm_values_left, bumper_active, rpm_activated
 
     ipcon.connect('localhost', 4223)
     time.sleep(0.5)
     servo = BrickServo('6JqqH8', ipcon)
-    servo.set_acceleration(0, 50000)  # right wheel
-    servo.set_velocity(0, 50000)
-    servo.set_acceleration(1, 50000)  # left wheel
-    servo.set_velocity(1, 50000)
     servo.set_acceleration(2, 30000)  # cutter
     servo.set_velocity(2, 30000)
     io16 = BrickletIO16('b7Y', ipcon)
@@ -49,7 +49,7 @@ def start(parent_conn):
     io16.set_edge_count_config(1, BrickletIO16.EDGE_TYPE_BOTH, 1)  # set pin 1 for edge count
     iqr = BrickletIndustrialQuadRelay('mT2', ipcon)
     iqr.set_monoflop(0b0111, 0b0111, 1500)
-    analog = BrickletAnalogIn('', ipcon)
+    analog = BrickletAnalogIn('bK7', ipcon)
     analog.set_range(BrickletAnalogIn.RANGE_UP_TO_6V)
 
     while True:
@@ -59,6 +59,7 @@ def start(parent_conn):
 
         # commands: forward/<speed>, backward/<speed>, turnL/, turnR/,
         # curveL/<smooth|medium|strong>, curveR/<smooth|medium|strong>, cutter/<speed>, stop/
+        # reset_bumper/
         if internal_cmd is not None:
             cmd = internal_cmd
             internal_cmd = None
@@ -70,32 +71,36 @@ def start(parent_conn):
             execute_command(cmd, servo)
 
         # check rpm per wheel against moving average to check if wheel is blocked
-        if (loop_counter % 8) == 0:
-            right_rpm = io16.get_edge_count(0, True)
-            left_rpm = io16.get_edge_count(1, True)
+        if (loop_counter % 4) == 0:
+            left_rpm = abs(io16.get_edge_count(1, True))
+            right_rpm = abs(io16.get_edge_count(0, True))
             rpm_values_right.append(right_rpm)
             rpm_values_left.append(left_rpm)
-
-            if len(rpm_values_right) > 5:  # omit first vales
+            if rpm_activated:
                 moving_average = sum(rpm_values_right) / len(rpm_values_right)
-                if right_rpm < (moving_average * 0.50):
+                if moving_average > 1.0 and right_rpm < (moving_average * 0.50):
+                    print("Average right", moving_average)
+                    print("Queue right", str(rpm_values_right))
                     logger.info("Right wheel blocked, stop mower")
                     internal_cmd = 'stop/'
 
-            if len(rpm_values_left) > 5:  # omit first vales
                 moving_average = sum(rpm_values_left) / len(rpm_values_left)
-                if left_rpm < (moving_average * 0.50):
-                    logger.info("Right wheel blocked, stop mower")
+                if moving_average > 1.0 and left_rpm < (moving_average * 0.50):
+                    print("Average left", moving_average)
+                    print("Queue left", str(rpm_values_left))
+                    logger.info("Left wheel blocked, stop mower")
                     internal_cmd = 'stop/'
 
         # check bumper
-        if ((loop_counter + 4) % 8) == 0:
+        if not bumper_active and ((loop_counter + 2) % 4) == 0:
             volt = analog.get_voltage()
             bumper_values.append(volt)
             moving_average = sum(bumper_values) / len(bumper_values)
             if volt > (moving_average * 1.3):
-                logger.info("Bumper activated, stop mower")
+                logger.info("Bumper triggered, stop mower")
                 internal_cmd = 'stop/'
+                bumper_active = True
+                parent_conn.send("bumper_active")
 
         # update drive monoflop
         if (loop_counter % 100) == 0:
@@ -105,46 +110,49 @@ def start(parent_conn):
 
 
 def execute_command(cmd, servo):
-    global cur_mode, cur_speed
+    global cur_mode, cur_speed, bumper_active, bumper_values
     split_cmd = cmd.split('/')
     cur_mode = split_cmd[0]
     if cur_mode == 'forward':
         cur_speed = int(split_cmd[1])
-        reset_queues('both')
         execute_drive_command(servo, cur_speed, cur_speed)
+        reset_rpm()
     elif cur_mode == 'backward':
         cur_speed = int(split_cmd[1])
-        reset_queues('both')
         execute_drive_command(servo, -cur_speed, -cur_speed)
+        reset_rpm()
     elif cur_mode == 'turnL':
-        reset_queues('both')
-        cur_speed = 3000
+        cur_speed = 3500
         execute_drive_command(servo, cur_speed, -cur_speed)
+        reset_rpm()
     elif cur_mode == 'turnR':
-        reset_queues('both')
-        cur_speed = 3000
+        cur_speed = 3500
         execute_drive_command(servo, -cur_speed, cur_speed)
+        reset_rpm()
     elif cur_mode == 'curveL':
-        reset_queues('left')
         if split_cmd[1] == 'smooth':
             execute_drive_command(servo, cur_speed, cur_speed - 400)
         elif split_cmd[1] == 'medium':
             execute_drive_command(servo, cur_speed, cur_speed - 700)
         elif split_cmd[1] == 'strong':
             execute_drive_command(servo, cur_speed, cur_speed - 1100)
+        reset_rpm()
     elif cur_mode == 'curveR':
-        reset_queues('right')
         if split_cmd[1] == 'smooth':
             execute_drive_command(servo, cur_speed - 400, cur_speed)
         elif split_cmd[1] == 'medium':
             execute_drive_command(servo, cur_speed - 700, cur_speed)
         elif split_cmd[1] == 'strong':
             execute_drive_command(servo, cur_speed - 1100, cur_speed)
+        reset_rpm()
     elif cur_mode == 'cutter':
         execute_cutter_command(servo, int(split_cmd[1]))
     elif cur_mode == 'stop':
-        reset_queues('both')
         execute_drive_command(servo, 0, 0)
+        reset_rpm()
+    elif cur_mode == 'reset_bumper':
+        bumper_active = False
+        bumper_values.clear()
 
 
 # execute a drive command
@@ -160,12 +168,17 @@ def execute_cutter_command(servo, cutter_speed):
     servo.enable(2)
 
 
-def reset_queues(name):
-    global rpm_values_right, rpm_values_left
-    if name == 'right':
-        rpm_values_right.clear()
-    elif name == 'left':
-        rpm_values_left.clear()
-    elif name == 'both':
-        rpm_values_right.clear()
-        rpm_values_left.clear()
+# activate the wheel rpm
+def activate_rpm():
+    global rpm_activated, rpm_values_left, rpm_values_right
+    rpm_values_right.clear()
+    rpm_values_left.clear()
+    rpm_activated = True
+
+
+# Activate rpm checks only after a delay to prevent blocked wheel error messages if directions change
+def reset_rpm():
+    global rpm_activated
+    rpm_activated = False
+    t = Timer(1.0, activate_rpm)
+    t.start()
